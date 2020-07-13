0c50be7857f79464daaa4762d6a3aa47a6744f13
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-07-13 18:02:39 -0400

    Mathtools.

--- trio/segments/Segment_A/definition.py
+++ trio/segments/Segment_A/definition.py
@@ -69,7 +69,7 @@ time_signatures = [
     ]
 ]
 
-bounds = abjad.mathtools.cumulative_sums([_.duration for _ in time_signatures])
+bounds = abjad.mathx.cumulative_sums([_.duration for _ in time_signatures])
 
 # Define Pitch Material
 
